181ab28dae61edb863cb67028d18a16ed9723888
Adlan Arif Zakaria <adlan@newsoft.com.my>
2021-01-13 23:26:23 +0800

    Fixed plugin not working with typescript

--- src/boot/index.js
+++ src/boot/index.js
@@ -1,4 +1,4 @@
-import QSwipeToClose from 'quasar-app-extension-swipe-to-close/src/components/SwipeToClose'
+import QSwipeToClose from 'quasar-app-extension-swipe-to-close/src/components/SwipeToClose.vue'
 
 export default ({ Vue }) => {
   Vue.component('q-swipe-to-close', QSwipeToClose)
